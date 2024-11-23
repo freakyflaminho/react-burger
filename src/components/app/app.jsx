79f117843a280384132dfd638148cfd5c4d223bd
freakyflaminho <mikhail.buyklu@utair.ru>
2024-11-23 22:05:41 +0300

    Created api connection for getting burger ingredients

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,17 +1,38 @@
+import { useEffect, useState } from 'react';
 import AppHeader from '../app-header/app-header';
 import ConstructorPage from '../pages/constructor-page/constructor-page';
-import ingredients from '../../utils/data.json';
 import selectedIngredientIds from '../../utils/selectedIngredientIds.json';
+import { getIngredients } from '../../utils/api';
 import styles from './app.module.css';
 
 const App = () => {
+
+  const [ingredients, setIngredients] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+      setIsLoading(true);
+      getIngredients()
+        .then(ingredients => {
+          setIngredients(ingredients);
+          setIsLoading(false);
+        })
+        .catch(err => {
+          setIsError(true);
+          setIsLoading(false);
+        });
+    }, []
+  );
+
   return (
     <div className={styles.container}>
       <AppHeader />
-      <ConstructorPage
-        ingredients={ingredients}
-        selectedIngredientIds={selectedIngredientIds}
-      />
+      {!isLoading && !isError &&
+        <ConstructorPage
+          ingredients={ingredients}
+          selectedIngredientIds={selectedIngredientIds} />
+      }
     </div>
   );
 };
