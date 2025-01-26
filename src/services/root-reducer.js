8be18e52380c81cc2ff024faaba567e0dc4b27b6
freakyflaminho <mikhail.buyklu@utair.ru>
2025-01-26 22:18:00 +0300

    Removed redundant ingredient details slice

--- src/services/root-reducer.js
+++ src/services/root-reducer.js
@@ -2,11 +2,9 @@ import { combineSlices } from '@reduxjs/toolkit';
 import { api } from './api/api';
 import { authSlice } from './slices/auth-slice';
 import { burgerConstructorSlice } from './slices/burger-constructor-slice';
-import { ingredientDetailsSlice } from './slices/ingredient-details-slice';
 
 export const rootReducer = combineSlices(
   api,
   authSlice,
   burgerConstructorSlice,
-  ingredientDetailsSlice,
 );
