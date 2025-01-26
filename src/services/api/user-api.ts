937ee50fa43765688eeb1d19bd94781399f3c4e8
freakyflaminho <mikhail.buyklu@utair.ru>
2025-01-26 23:17:57 +0300

    Fixed bug when after logout user was authorized until refresh page

--- src/services/api/user-api.ts
+++ src/services/api/user-api.ts
@@ -40,7 +40,7 @@ export const userApi = api.injectEndpoints({
         body: { token: getRefreshToken() },
       }),
       async onQueryStarted(_, { dispatch, queryFulfilled }) {
-        dispatch(setAuth(true));
+        dispatch(setAuth(false));
         await queryFulfilled.finally(() => {
           removeTokens();
         });
